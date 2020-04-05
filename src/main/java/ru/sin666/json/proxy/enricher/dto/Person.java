38b404ab6e15e34893cfcefdc3aca74cae7b80b5
sin999 <sinpro@ya.ru>
2020-04-05 11:29:11 +0300

    init commit

---
+++ src/main/java/ru/sin666/json/proxy/enricher/dto/Person.java
@@ -0,0 +1,14 @@
+package ru.sin666.json.proxy.enricher.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+}
